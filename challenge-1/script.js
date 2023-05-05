255509879d5ddf4d44e32e466db02f573a9a15b5
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-05-05 13:30:49 +0200

    Update script.js

--- challenge-1/script.js
+++ challenge-1/script.js
@@ -70,77 +70,30 @@ console.log('ID change:', holidays[christmas].id !== copied.id)
 console.log('Name change:', holidays[christmas].name !== copied.name || copied.name)
 console.log('Date change:', holidays[christmas].date !== copied.date || copied.date)
 
-const holidaysCopy = {
-    0: {
-        id: 0,
-        name: 'Day of Reconciliation',
-        date: new Date(`16 December ${currentYear}`),
-    },
-    1: {
-        id: 1,
-        name: 'Workers Day',
-        date: new Date(`1 April ${currentYear}`),
-    },
-    2: {
-        id: 2,
-        name: 'Day of Goodwill',
-        date: new Date(`26 December ${currentYear}`),
-    },
-    3: {
-        id: 3,
-        name: 'New Year Day',
-        date: new Date(`1 January ${currentYear}`),
-    },
-    4: {
-        id: 4,
-        name: 'Womens Day',
-        date: new Date(`9 August ${currentYear}`),
-    },
-    5: {
-        id: 5,
-        name: 'Heritage Day',
-        date: new Date(`24 September ${currentYear}`),
-    },
-    6: {
-        id: 6,
-        name: 'Christmas Day',
-        date: new Date(`25 December ${currentYear} 13:25`),
-    },
-    7: {
-        id: 7,
-        name: 'Youth Day',
-        date: new Date(`16 June ${currentYear}`),
-    },
-    8: {
-        id: 8,
-        name: 'Human Rights Day',
-        date: new Date(`21 March ${currentYear}`)
-    },
-}
 
 
 const firstHolidayTimestamp = Math.min(
-    new Date(holidaysCopy[0].date).getTime(),
-    new Date(holidaysCopy[1].date).getTime(),
-    new Date(holidaysCopy[2].date).getTime(),
-    new Date(holidaysCopy[3].date).getTime(),
-    new Date(holidaysCopy[4].date).getTime(),
-    new Date(holidaysCopy[5].date).getTime(),
-    new Date(holidaysCopy[6].date).getTime(),
-    new Date(holidaysCopy[7].date).getTime(),
-    new Date(holidaysCopy[8].date).getTime(),
+    new Date(holidays[0].date).getTime(),
+    new Date(holidays[1].date).getTime(),
+    new Date(holidays[2].date).getTime(),
+    new Date(holidays[3].date).getTime(),
+    new Date(holidays[4].date).getTime(),
+    new Date(holidays[5].date).getTime(),
+    new Date(holidays[6].date).getTime(),
+    new Date(holidays[7].date).getTime(),
+    new Date(holidays[8].date).getTime(),
 ); console.log(firstHolidayTimestamp)
 
 const lastHolidayTimestamp = Math.max(
-    new Date(holidaysCopy[0].date).getTime(),
-    new Date(holidaysCopy[1].date).getTime(),
-    new Date(holidaysCopy[2].date).getTime(),
-    new Date(holidaysCopy[3].date).getTime(),
-    new Date(holidaysCopy[4].date).getTime(),
-    new Date(holidaysCopy[5].date).getTime(),
-    new Date(holidaysCopy[6].date).getTime(),
-    new Date(holidaysCopy[7].date).getTime(),
-    new Date(holidaysCopy[8].date).getTime(),
+    new Date(holidays[0].date).getTime(),
+    new Date(holidays[1].date).getTime(),
+    new Date(holidays[2].date).getTime(),
+    new Date(holidays[3].date).getTime(),
+    new Date(holidays[4].date).getTime(),
+    new Date(holidays[5].date).getTime(),
+    new Date(holidays[6].date).getTime(),
+    new Date(holidays[7].date).getTime(),
+    new Date(holidays[8].date).getTime(),
 ); console.log(lastHolidayTimestamp)
 
 const firstDay = new Date(firstHolidayTimestamp).getDate(); console.log(firstDay)
